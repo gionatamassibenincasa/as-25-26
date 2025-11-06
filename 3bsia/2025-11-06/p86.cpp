ab7696e4e13acf8df5dfd88acc11ceeac5ee293c
Gionata Massi <gionata.massi@savoiabenincasa.it>
2025-11-06 10:37:52 +0100

    Aggiunti esempi lezione 6 nov

---
+++ 3bsia/2025-11-06/p86.cpp
@@ -0,0 +1,14 @@
+// somma.cpp: somma di due numeri
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, s;
+    cout << "Inserisci due numeri interi: ";
+    cin >> a;
+    cin >> b;
+    s = a + b;
+    cout << s << endl;
+
+    return 0;
+}
---
+++ 3bsia/2025-11-06/p86.py
@@ -0,0 +1,6 @@
+# somma.py: somma di due numeri
+print ("Inserisci due numeri interi: ")
+a=int(input())
+b=int(input())
+s = a + b
+print(s)
